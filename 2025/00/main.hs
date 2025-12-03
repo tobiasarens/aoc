323be88135df0e155cda87d35442d6516fb4186b
Tobias Arens <tobiasgarens@gmail.com>
2025-12-03 22:20:36 +0100

    update template

---
+++ 2025/00/main.hs
@@ -0,0 +1,28 @@
+import Data.List.Split
+
+main :: IO()
+main = long 2
+
+run :: String -> IO()
+run file = do
+    putStrLn "Part 1"
+    ls <- splitOn "," . concat . lines <$> readFile file
+    print ls
+    return ()
+
+run2 :: String -> IO()
+run2 file = do
+    putStrLn "Part 2"
+    ls <- splitOn "," . concat . lines <$> readFile file
+    print ls
+    return ()
+
+short :: Int -> IO()
+short part
+    | part == 1 = run "s.txt"
+    | otherwise = run2 "s.txt"
+
+long :: Int ->  IO()
+long part
+    | part == 1 = run "l.txt"
+    | otherwise = run2 "l.txt"
