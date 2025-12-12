d0cc9714e3d1c40069b3277bc40dc7cff8790d54
Tobias Arens <tobias.arens@rwth-aachen.de>
2025-12-12 14:22:20 +0100

    add day 10 part 1

--- 2025/10/main.hs
+++ 2025/10/main.hs
@@ -8,21 +8,37 @@ import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-import Data.PQueue.Prio.Min (MinPQueue)
-import qualified Data.PQueue.Prio.Min as PQ
+import Data.Heap (Heap)
+import qualified Data.Heap as Heap
 
 main :: IO()
-main = long 2
+main = long 1
 
 run :: String -> IO()
 run file = do
     putStrLn "Part 1"
     ls <-  lines <$> readFile file
-    -- let machines = map parseMachine ls
-    -- let shortest = map shortestSol machines
-    -- print $ sum shortest
+    let machines = map parseMachine ls
+
+    shortest <- solveMachine 1 machines
+
+    --let shortest = map ((+ negate 1) . length . flip shortestPath 0) machines
+
+    --print shortest
+    print $ sum shortest
 
     return ()
+
+solveMachine :: Int -> [Machine] -> IO([Int])
+solveMachine _ [] = return []
+solveMachine num (m:remain) = do
+            let sol = length (shortestPath m 0) - 1
+            putStrLn $ "Nr: " ++ show num ++ " - result: " ++show sol ++" - " ++ show m
+            rest <- solveMachine (num+1) remain
+            return (sol : rest)
+
+
+
 
 run2 :: String -> IO()
 run2 file = do
@@ -97,10 +113,6 @@ parseMachine line = (lCount, target, buttons)
                     | h == "(" || h == ")" =  parseWord tl
                     | otherwise = read h : parseWord tl
 
-
-solveMachine :: Machine -> Int
-solveMachine _ = 0
-
 data Distance a = Dist a | Infinity
     deriving (Show, Eq)
 
@@ -114,45 +126,86 @@ addDist :: (Num a) => Distance a -> Distance a -> Distance a
 addDist (Dist x) (Dist y) = Dist (x+y)
 addDist _ _ = Infinity
 
-data SearchState = SearchState {
+data BFSState = BFS {
     visited :: Set Lights,
-    prec :: Map Lights (Lights, Int),
-    distances :: Map Lights (Distance Int),
-    queue :: MinPQueue (Distance Int) Lights
+    queue :: [Lights],
+    -- queue :: Heap (Entry Int Lights),
+    prec :: Map Lights Lights,
+    found :: Bool
 }
 
-instance Show SearchState where
-    show (SearchState v p d q) = "visited: " ++ (show v) ++ "\n" ++ "distances: " ++ (show d) ++ "\n" ++ "precs: " ++ (show p) ++ "\n"
+instance Show BFSState where
+    show (BFS v q p f) = "visited: " ++ show v ++ "\nqueue: " ++ show q ++ "\nprec: " ++ show p ++ "\nFINISHED: " ++ show f ++ "\n"
 
-emptySearchState = SearchState Set.empty Map.empty Map.empty PQ.empty
-startSearchState light = SearchState (Set.singleton light) (Map.singleton light (0, 0)) (Map.singleton light (Dist 0)) PQ.empty
+startBFSState :: Lights -> BFSState
+startBFSState start = BFS Set.empty [start] Map.empty False
 
-reachable :: Machine -> Lights -> [(Lights, Int)]
-reachable (_, _, btns) light = [((btns !! i) light, i) | i <-[0..length btns - 1]]
-
-traverse :: Machine -> SearchState -> Lights -> SearchState
-traverse m ss l = ss
-
-updateState :: Lights -> (Lights, Int) -> SearchState -> SearchState 
-updateState origin (light, withBtn) (SearchState v p d q) 
-    | Set.member light v = SearchState v p' d' q
-    | otherwise = SearchState (Set.insert light v) (Map.insert light (origin, withBtn) p ) (Map.insert light (addDist (d Map.! origin) (Dist 1)) d) q
+bfsStep :: Machine -> BFSState -> BFSState
+bfsStep machine (BFS v q p f)
+    | f = BFS v q p f
+    | L.null q = BFS v q p False
+    | found = BFS (Set.insert current v) q precs True
+    | otherwise = BFS (Set.insert current v) queue' precs f
     where
-        currDist = d Map.! light
-        originDist = d Map.! origin
+        (c, target, btns) = machine
+        (current:queue) = q
+        reach = reachable btns current
+        found = target `elem` reach
+        queue' = queue ++ reach
+        precs = Map.union p $ Map.fromList [(r, current) | r <- reach]
 
-        (p', d') = if currDist >= originDist
-                then (p, d)
-                else (Map.insert light (origin, withBtn) p, Map.insert light (addDist originDist (Dist 1)) d)
+bfs :: Machine -> BFSState -> BFSState
+bfs machine (BFS v q p f)
+    | f = BFS v q p f
+    | L.null q = BFS v q p False
+    | Set.member current v = bfs machine (BFS v queue p f)
+    | found = BFS (Set.insert current v) q (Map.insert target current p) True
+    | otherwise = bfs (c, target, btns) nextState
+    where
+        (c, target, btns) = machine
+        (current:queue) = q
+        reach = reachable btns current
+        found = target `elem` reach
+        nextState = BFS (Set.insert current v) (queue ++ reach) (foldl' (\acc l -> Map.insert l current acc) p reach) f
 
+bfs' :: Machine -> BFSState -> BFSState
+bfs' machine (BFS v q p f)
+    | f = BFS v q p f
+    | L.null q = BFS v q p False
+    | otherwise = bfs' machine $ bfsStep machine (BFS v q p f)
 
-exploreAll :: Machine -> Lights -> SearchState -> SearchState
-exploreAll (c, target, btns) pos (SearchState v p d q) 
-    | pos == target = keepMin (SearchState v p d q) 
-    | Set.member pos v = SearchState v p d q
+steps :: Int -> BFSState
+steps 0 = startBFSState 0
+steps i = bfsStep m2 $ steps (i-1)
 
-shortestPath :: Machine -> Lights -> Lights -> [Int]
-shortestPath m start stop = []
+bfsSolution :: BFSState -> Lights -> String
+bfsSolution bfs l
+    | Map.member l p = show l ++ "->" ++ bfsSolution bfs (p Map.! l)
+    | otherwise = show l
+    where
+        BFS _ _ p _ = bfs
+
+bfsPath :: BFSState -> Lights ->  Lights -> [Lights]
+bfsPath bfs start l
+    | start == l = [l]
+    | Map.member l p = l:bfsPath bfs start (p Map.! l)
+    | otherwise = [l]
+    where
+        (BFS _ _ p _) = bfs
+
+reachable :: [Button] -> Lights -> [Lights]
+reachable btns l = [b l | b <- btns]
+
+r' :: Lights -> [Lights]
+r' l = reachable btns l
+    where
+        (_, _, btns) = m1
+
+shortestPath :: Machine -> Lights -> [Lights]
+shortestPath m start = bfsPath bfsResult start target
+    where
+        (_, target, _) = m
+        bfsResult = bfs' m (startBFSState start)
 
 findShortest :: Machine -> Distance Int
 findShortest m = Infinity
@@ -161,4 +214,6 @@ m1 = parseMachine "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
 b6 :: Button
 (c1, l1, [b1, b2, b3, b4, b5, b6]) = m1
 
-m2 = parseMachine "[#..#.#..##] (2,8) (1,4,6) (0,1,2,3,7,8,9) (0,1,3,4,5,6,8,9) (0,1,2,3,4,6,7) (2,3,5,6,8,9) (0,1,2,4,5,7,9) (0,1,4) {67,81,56,42,62,16,37,46,31,35}"
+m2 = parseMachine "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}"
+
+p2 = parseMachine "[#..#.#..##] (2,8) (1,4,6) (0,1,2,3,7,8,9) (0,1,3,4,5,6,8,9) (0,1,2,3,4,6,7) (2,3,5,6,8,9) (0,1,2,4,5,7,9) (0,1,4) {67,81,56,42,62,16,37,46,31,35}"
